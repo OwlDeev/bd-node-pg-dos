73b3bead18e9448858980197af66c3752587bc54
OwlDeev <82920284+OwlDeev@users.noreply.github.com>
2023-01-13 01:31:29 -0400

    add control error link

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
+app.use('*', function(req, res) {
+    res.send("Error. Intente nuevamente con otra ruta.");
+});
+
 app.listen(3000, console.log("SERVIDOR ENCENDIDO"));
